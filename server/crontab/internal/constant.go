87af5ccb41c5bfef2aa17854e1d5f6c3288f7765
thxyh99 <305095253@qq.com>
2024-04-22 15:40:18 +0800

    【重要】从2022年8月15日10点开始，“企业管理后台 - 管理工具 - 通讯录同步”的新增IP将不能再调用此接口，企业可通过「获取部门ID列表」接口获取部门ID列表。

--- server/crontab/internal/constant.go
+++ server/crontab/internal/constant.go
@@ -2,9 +2,9 @@ package internal
 
 const (
 	// CorpId 测试
-	CorpId = "wx5c17dffdd35452fa"
-	Secret = "iz2-5ArQY-OeQbKnvUKCpV-b-6mtLDItZrCKujK_Pxw"
+	//CorpId = "wx5c17dffdd35452fa"
+	//Secret = "iz2-5ArQY-OeQbKnvUKCpV-b-6mtLDItZrCKujK_Pxw"
 	// 正式
-	// CorpId = "wwd2315d04fd027aa5"
-	// Secret = "eIkxU8PXq9l7mPioYDYVHBdZhCWmdo62F4VZ6zJJzu4"
+	CorpId = "wwd2315d04fd027aa5"
+	Secret = "eIkxU8PXq9l7mPioYDYVHBdZhCWmdo62F4VZ6zJJzu4"
 )
--- server/crontab/service.go
+++ server/crontab/service.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/flipped-aurora/gin-vue-admin/server/crontab/internal"
+	"github.com/flipped-aurora/gin-vue-admin/server/utils"
 	"io/ioutil"
 	"net/http"
 	"time"
@@ -24,31 +25,100 @@ type AccessTokenResponse struct {
 	ErrMsg      string `json:"errmsg"`
 }
 
+type Department struct {
+	ID               int      `json:"id"`
+	Name             string   `json:"name"`
+	NameEN           string   `json:"name_en"`
+	DepartmentLeader []string `json:"department_leader"`
+	ParentID         int      `json:"parentid"`
+	Order            int      `json:"order"`
+}
+
 func (s *Service) SyncDepartment() {
 	fmt.Println("Sync department success:", time.Now())
 	accessToken, tokenErr := s.getToken()
 	if tokenErr != nil {
 		return
 	}
-	//获取部门列表
-	url := fmt.Sprintf("https://qyapi.weixin.qq.com/cgi-bin/department/list?access_token=%s", accessToken)
+	//获取部门列表,废弃了不让请求
+	//url := fmt.Sprintf("https://qyapi.weixin.qq.com/cgi-bin/department/list?access_token=%s", accessToken)
 	//获取子部门ID列表
-	//url := fmt.Sprintf("https://qyapi.weixin.qq.com/cgi-bin/department/simplelist?access_token=%s", accessToken)
-	resp, err := http.Get(url)
+	url := fmt.Sprintf("https://qyapi.weixin.qq.com/cgi-bin/department/simplelist?access_token=%s", accessToken)
+	body, err := utils.SendGetRequest(url)
 	if err != nil {
-		fmt.Println("Error sending HTTP request:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	// 读取响应数据
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
 		return
 	}
 
-	fmt.Println("Response:", string(body))
+	var data map[string]interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println("Error decoding JSON:", err)
+		return
+	}
+
+	// 获取 department_id 数组
+	departmentID, ok := data["department_id"].([]interface{})
+	if !ok {
+		fmt.Println("Error getting department_id")
+		return
+	}
+
+	// 遍历 department_id 数组
+	for _, department := range departmentID {
+		departmentMap, ok := department.(map[string]interface{})
+		if !ok {
+			fmt.Println("Error converting department to map")
+			continue
+		}
+		id := departmentMap["id"].(float64)
+		parentID := departmentMap["parentid"].(float64)
+		order := departmentMap["order"].(float64)
+		fmt.Printf("ID: %d, ParentID: %d, Order: %d\n", int(id), int(parentID), int(order))
+
+		////获取单个部门详情
+		// 从2022年8月15日10点开始，新增IP不能访问了
+		//urlInfo := fmt.Sprintf("https://qyapi.weixin.qq.com/cgi-bin/department/get?access_token=%s&id=%s", accessToken, id)
+		//bodyInfo, err := utils.SendGetRequest(urlInfo)
+		//if err != nil {
+		//	return
+		//}
+		//var dataInfo map[string]interface{}
+		//err = json.Unmarshal(bodyInfo, &dataInfo)
+		//if err != nil {
+		//	fmt.Println("Error decoding JSON:", err)
+		//	return
+		//}
+		//
+		//fmt.Println(dataInfo)
+		//
+		//// 获取 department 数组
+		//departmentData, ok := dataInfo["department"].(map[string]interface{})
+		//fmt.Println(departmentData)
+		//if !ok {
+		//	fmt.Println("Error getting department")
+		//	return
+		//}
+		//// 解析 department 字段到 Department 结构体
+		//var department Department
+		//departmentJSON, err := json.Marshal(departmentData)
+		//if err != nil {
+		//	fmt.Println("Error decoding department data:", err)
+		//	return
+		//}
+		//err = json.Unmarshal(departmentJSON, &department)
+		//if err != nil {
+		//	fmt.Println("Error decoding department JSON:", err)
+		//	return
+		//}
+		//
+		//// 输出解析结果
+		//fmt.Printf("Department ID: %d\n", department.ID)
+		//fmt.Printf("Department Name: %s\n", department.Name)
+		//fmt.Printf("Department Name (EN): %s\n", department.NameEN)
+		//fmt.Printf("Department Leaders: %v\n", department.DepartmentLeader)
+		//fmt.Printf("Department Parent ID: %d\n", department.ParentID)
+		//fmt.Printf("Department Order: %d\n", department.Order)
+	}
 
 }
 
