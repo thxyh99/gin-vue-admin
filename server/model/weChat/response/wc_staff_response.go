17879c4d1a30152c2fd15397aaf93cdda0be8106
thxyh99 <305095253@qq.com>
2024-05-10 14:14:05 +0800

    优化，多携程处理数据

--- server/model/weChat/response/wc_staff_response.go
+++ server/model/weChat/response/wc_staff_response.go
@@ -7,6 +7,7 @@ import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/weChat"
 	"github.com/flipped-aurora/gin-vue-admin/server/utils"
+	"sync"
 )
 
 // WcStaffResponse 账号信息 结构体
@@ -24,6 +25,7 @@ type WcStaffResponse struct {
 func (WcStaffResponse) Assemble(staffs []weChat.WcStaff) (newStaffs []WcStaffResponse) {
 	var newStaff WcStaffResponse
 	configInfo := config.GetConfigInfo()
+
 	for _, item := range staffs {
 		newStaff.WcStaff = item
 		gender, _ := utils.Find(configInfo.StaffGender, *item.Gender)
@@ -33,66 +35,74 @@ func (WcStaffResponse) Assemble(staffs []weChat.WcStaff) (newStaffs []WcStaffRes
 		status, _ := utils.Find(configInfo.StaffStatus, *item.Status)
 		newStaff.StatusText = status
 
-		//拼接员工职位信息
-		var sPosition weChat.WcStaffPosition
-		var positionIds []int
-		var positionText string
-		pRows, err := global.GVA_DB.Table(sPosition.TableName()+" as sp").Select("p.id,p.name").
-			Joins("left join wc_position as p on p.id = sp.position_id").Where("sp.staff_id=?", item.ID).Rows()
-		if err != nil {
-			fmt.Println("position1 err:", err)
-		} else {
-			for pRows.Next() {
-				var id int
-				var name string
-				err = pRows.Scan(&id, &name)
-				if err != nil {
-					fmt.Println("position2 err:", err)
-				} else {
-					positionIds = append(positionIds, id)
-					if positionText != "" {
-						positionText += ";" + name
+		var wg = sync.WaitGroup{}
+		wg.Add(2)
+		go func(item weChat.WcStaff) {
+			defer wg.Done()
+
+			//拼接员工职位信息
+			var sPosition weChat.WcStaffPosition
+			var positionIds []int
+			var positionText string
+			pRows, err := global.GVA_DB.Table(sPosition.TableName()+" as sp").Select("p.id,p.name").
+				Joins("left join wc_position as p on p.id = sp.position_id").Where("sp.staff_id=?", item.ID).Rows()
+			if err != nil {
+				fmt.Println("position1 err:", err)
+			} else {
+				for pRows.Next() {
+					var id int
+					var name string
+					err = pRows.Scan(&id, &name)
+					if err != nil {
+						fmt.Println("position2 err:", err)
 					} else {
-						positionText = name
+						positionIds = append(positionIds, id)
+						if positionText != "" {
+							positionText += ";" + name
+						} else {
+							positionText = name
+						}
 					}
 				}
 			}
-		}
-		newStaff.PositionIds = positionIds
-		newStaff.Position = positionText
+			newStaff.PositionIds = positionIds
+			newStaff.Position = positionText
+		}(item)
 
-		//拼接员工部门信息
-		var sDepartment weChat.WcStaffDepartment
-		var departmentIds []int
-		var departmentText string
-		//dRows, err := global.GVA_DB.Table(sDepartment.TableName()+" as sd").Select("d.id").
-		//	Joins("left join wc_department as d on d.id = sd.department_id").Where("sd.staff_id=?", item.ID).Rows()
-		dRows, err := global.GVA_DB.Table(sDepartment.TableName()).Select("department_id").Where("staff_id=?", item.ID).Rows()
-		if err != nil {
-			fmt.Println("department1 err:", err)
-		} else {
-			for dRows.Next() {
-				var departmentId int
-				err = dRows.Scan(&departmentId)
-				fullName := weChat.GetFullDepartmentById(departmentId)
-				if err != nil {
-					fmt.Println("department2 err:", err)
-				} else {
-					departmentIds = append(departmentIds, departmentId)
-					if departmentText != "" {
-						departmentText += ";" + fullName
+		go func(item weChat.WcStaff) {
+			defer wg.Done()
+
+			//拼接员工部门信息
+			var sDepartment weChat.WcStaffDepartment
+			var departmentIds []int
+			var departmentText string
+			dRows, err := global.GVA_DB.Table(sDepartment.TableName()).Select("department_id").Where("staff_id=?", item.ID).Rows()
+			if err != nil {
+				fmt.Println("department1 err:", err)
+			} else {
+				for dRows.Next() {
+					var departmentId int
+					err = dRows.Scan(&departmentId)
+					fullName := weChat.GetFullDepartmentById(departmentId)
+					if err != nil {
+						fmt.Println("department2 err:", err)
 					} else {
-						departmentText = fullName
+						departmentIds = append(departmentIds, departmentId)
+						if departmentText != "" {
+							departmentText += ";" + fullName
+						} else {
+							departmentText = fullName
+						}
 					}
 				}
 			}
-		}
-		newStaff.DepartmentIds = departmentIds
-		newStaff.Department = departmentText
+			newStaff.DepartmentIds = departmentIds
+			newStaff.Department = departmentText
+		}(item)
 
+		wg.Wait()
 		newStaffs = append(newStaffs, newStaff)
 	}
-
 	return
 }
 
